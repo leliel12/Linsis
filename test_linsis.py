01988a7028077d86e02f8ba7b0e0b2c0ec8df955
Juan BC <jbc.develop@gmail.com>
2020-10-07 11:17:17 -0300

    Update test_linsis.py

--- test_linsis.py
+++ test_linsis.py
@@ -1,31 +1,61 @@
-from Linsis import spectro
+# licencia
+
+# ==============================================================================
+# DOCS
+# ==============================================================================
+
+"""Linsis test suite."""
+
+
+# ==============================================================================
+# IMPORTS
+# ==============================================================================
+
+import os
+import pathlib
+
 import pytest
 
-class Test_Load_spectrum():
+from Linsis import Spectrum
 
-#def not_empty(spec):
-#	if spec is empty rise error
-#import pytest
 
-    @pytest.fixture
-    def spectrum(self):
-        spect = spectro('/home/fenix/Documents/WorkWorkWork/NGC4945/esp_finales/cfgal01.0001.fits',0)
-        return spect
-   
-    def test_match(self, spectrum):
-        assert spectrum.spectral_axis.shape == spectrum.flux.shape
-	
-	
-    #def test_unities(self):
-    #	assert u.spec.spectral_axis = 'AA'
-    #	assert u.spec.flux = 'erg cm-2 s-1 AA-1'	
-	
-	
-    def test_header(self, spectrum):
-        assert type(spectrum.header['EXPTIME']) == float
-        assert spectrum.header['EXPTIME'] >= 0.
-        
-        
-    def test_wav_axis(self, spectrum):
-        assert spectrum.header['CD1_1'] >=0.
-        assert spectrum.header['CTYPE1'] == 'LINEAR'
+# ==============================================================================
+# CONSTANTS
+# ==============================================================================
+
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+
+TEST_PATH = pathlib.Path(PATH) / "test_data"
+
+
+# ==============================================================================
+# FIXTURES
+# ==============================================================================
+
+@pytest.fixture(scope="session")
+def spectrum160209():
+    path = TEST_PATH / "nqgem-s-160209-4542.fits"
+    spect = Spectrum(path, 0)
+    return spect
+
+
+# ==============================================================================
+# TESTS
+# ==============================================================================
+
+def test_match(spectrum160209):
+    spectrum = spectrum160209
+    assert spectrum.spectral_axis.shape == spectrum.flux.shape
+
+
+def test_header(spectrum160209):
+    spectrum = spectrum160209
+    assert isinstance(spectrum.header['EXPTIME'], (float, int))
+    assert spectrum.header['EXPTIME'] >= 0.
+
+
+def test_wav_axis(spectrum160209):
+    spectrum = spectrum160209
+    assert spectrum.header['CD1_1'] >= 0.
+    assert spectrum.header['CTYPE1'] == 'LINEAR'
